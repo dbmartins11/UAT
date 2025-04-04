638d180d967dc9810f43e06ff40df7a9967aece4
dbmartins11 <gomartins5@hotmail.com>
2025-04-04 21:03:35 +0200

    feat: implement city fetching functionality and update home component

--- Ubiqutous/api/api.js
+++ Ubiqutous/api/api.js
@@ -1,1 +1,35 @@
 // https://restcountries.com/v3.1/all?fields=name 
+//import SerpApi from 'google-search-results-nodejs';
+
+const serpapiKey = "9a0b8b66a8c2ff39a093921cc324852293c5e052eed48a7e65be066e3db34a3d";
+
+
+const popularTouristCountries = [
+    "Portugal", "France", "Spain", "USA", "China", "Italy", "Turkey",
+    "Mexico", "Germany", "Thailand", "Greece", "Japan", "Brazil",
+];
+
+export const fetchCities = async (country) => {
+    const cities = [];
+    try {
+        const response = await fetch(`https://serpapi.com/search.json?engine=google&q=Cities+in+${country}&api_key=${serpapiKey}`);
+        const data = await response.json();
+        const table = data.related_questions.find(q =>
+            q.table !== undefined
+        ).table;
+
+        table.forEach(element => {
+            let city = element[0];
+            if(city !== "City"){
+                cities.push(city);
+            }
+        });
+        console.log("adasdasdasdasd: " + cities);
+        return data
+    }
+    catch (error) {
+        console.error('Error fetching cities:', error);
+        return null;
+    }
+}
+
--- Ubiqutous/app/(tabs)/home.js
+++ Ubiqutous/app/(tabs)/home.js
@@ -1,35 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet, ImageBackground, Image} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
+import { fetchCities } from '../../api/api';
 
 export default function Home() {
+    useEffect(() => {
+        console.log('Home component mounted'); 
+        const fetchData = async () => {
+            try {
+                fetchCities("Portugal")
+            }
+            catch (error) {
+                console.error('Error in Home component:', error);
+            };
+        }
+        fetchData(); 
+    }, []);
+
     return (
         <View style={styles.homeImage}>
             <ImageBackground
                 source={require('../../assets/images/HomePicture.png')}
                 style={styles.imageBackground}>
-                <Text>Plan your Next </Text>
-                <Text>Vacation</Text>
+                <Text style={styles.slogan}>Plan your Next </Text>
+                <Text style={styles.slogan}>Vacation</Text>
                 <Image
                     source={require('../../assets/images/icon.png')}
                     style={styles.icon} />
             </ImageBackground>
         </View>
     );
-}    
+}
 
 const styles = StyleSheet.create({
     homeImage: {
         height: '47%',
         width: '100%',
-        textAlign:'left',
+        textAlign: 'left',
         fontFamily: 'PatuaOne',
-        fontSize: 100,
     },
     icon: {
         width: '20%',
-        height:'20%',
+        height: '20%',
         left: '40%',
-        bottom: '35%',
+        bottom: '40%',
     },
     imageBackground: {
         flex: 1,
@@ -39,4 +52,11 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    slogan: {
+        textAlign: 'left',
+        fontSize: 35,
+        bottom: '15%',
+        width: '100%',
+        paddingLeft: 20,
+    }
 });
