4ccc2270f130591eb8150067aa24731fc7f5bd5e
dbmartins11 <gomartins5@hotmail.com>
2025-05-13 01:26:59 +0200

    feat: adding a new api to get images

--- Ubiqutous/api/api.js
+++ Ubiqutous/api/api.js
@@ -2,11 +2,15 @@
 //import SerpApi from 'google-search-results-nodejs';
 
 const serpapiKey = "9a0b8b66a8c2ff39a093921cc324852293c5e052eed48a7e65be066e3db34a3d";
+const unsplashKeyAccess = "krp2JlVVLvA0dtFnG4gLRV1DALKg-YNGslcYxWn5YO8";
+const unsplashKeySecret = "Ay8ZfLqVAPSuDO_ZXnF9iWa-4_qQef6Pqla-nIxd70w";
 
 export const fetchCities = async (country) => {
     const cities = [];
     try {
-        const response = await fetch(`https://serpapi.com/search.json?engine=google&q=Cities+in+${country}&api_key=${serpapiKey}`);
+        const url = `https://serpapi.com/search.json?engine=google&q=Cities+in+${country}&api_key=${serpapiKey}`;
+        const response = await fetch(url);
+        //const response = await fetch(`https://serpapi.com/search.json?engine=google&q=Cities+in+${country}&api_key=${serpapiKey}`);
         const data = await response.json();
         const table = data.related_questions.find(q =>
             q.table !== undefined
@@ -30,20 +34,41 @@ export const fetchCities = async (country) => {
 export const fetchImages = async (query) => {
     const images = [];
     try {
-        const response = await fetch(`https://serpapi.com/search.json?engine=google_images&q=${query}+Monumentos&api_key=${serpapiKey}`);
+        const url = `https://serpapi.com/search.json?engine=google_images&q=${query}+monumentos&api_key=${serpapiKey}`
+        const response = await fetch(url);
         const data = await response.json();
+        console.log('URL: ' + url);
+        console.log("API IMAGES: " + data);
 
         const links = data.images_results
         .filter(q =>q.thumbnail !== undefined)
         .map(q => q.thumbnail);
-
+        
         //console.log("adasdasdasdasd: " + links);
         //const link = links[0];
         
         return links;
     }
     catch (error) {
-        console.error('Error fetching cities:', error);
+        console.error('Error fetching images:', error);
+        return null;
+    }
+}
+
+
+export const fetchImagesUnsplash = async (city) => {
+    try{
+        const response = await fetch(`https://api.unsplash.com/search/photos?query=${city}`, {
+            headers: {
+                Authorization: `Client-ID ${unsplashKeyAccess}`
+            }
+        });
+        const data = await response.json();
+        const urls = data.results.map(res => res.urls.raw);
+        console.log("API IMAGES UNSPLASH: " + JSON.stringify(urls, null, 2));
+        return urls;
+    }catch (error) {
+        console.error('Error fetching images Unsplash:', error);
         return null;
     }
 }
@@ -56,11 +81,12 @@ export const fetchMonuments = async (city) => {
         const monuments = data.top_sights.sights
         .filter(q => q.title !== undefined)
         .map(q => q.title);
-        console.log("adasdasdasdasd: " + monuments );
+        console.log("API MONUMENTS: " + monuments );
         return monuments;
     }
     catch (error) {
-        console.error('Error fetching cities:', error);
+        console.error('Error fetching monuments:', error);
         return null;
     }
 }
+ 
