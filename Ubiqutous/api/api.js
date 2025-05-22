4bb7780f8e89a6646e14688a9e1f03e48e954df2
dbmartins11 <gomartins5@hotmail.com>
2025-05-22 21:53:01 +0200

    feat: create a new api method to get the monument description

--- Ubiqutous/api/api.js
+++ Ubiqutous/api/api.js
@@ -91,4 +91,22 @@ export const fetchMonuments = async (city) => {
         return null;
     }
 }
+
+export const fetchMonumentDescription = async (monumentName) => {
+  const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(monumentName)}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (data.extract) {
+      return data.extract;
+    } else {
+      return "Descrição não encontrada.";
+    }
+  } catch (error) {
+    console.error("Erro ao buscar descrição:", error);
+    return null;
+  }
+};
  
