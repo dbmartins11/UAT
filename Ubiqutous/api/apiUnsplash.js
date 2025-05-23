cf02163df8b8fbe7fc85750073f423679a2ce2f2
dbmartins11 <gomartins5@hotmail.com>
2025-05-24 00:19:35 +0200

    fix: changing the image to improve performance

--- Ubiqutous/api/apiUnsplash.js
+++ Ubiqutous/api/apiUnsplash.js
@@ -2,19 +2,21 @@ const unsplashKeyAccess = "krp2JlVVLvA0dtFnG4gLRV1DALKg-YNGslcYxWn5YO8";
 const unsplashKeySecret = "Ay8ZfLqVAPSuDO_ZXnF9iWa-4_qQef6Pqla-nIxd70w";
 
 export const fetchImagesUnsplash = async (city) => {
-    try{
+    try {
         const url = `https://api.unsplash.com/search/photos?query=${city}`;
         const response = await fetch(url, {
             headers: {
                 Authorization: `Client-ID ${unsplashKeyAccess}`
             }
         });
-        
-        console.log('URL: ' + url);
+
+        //console.log('URL: ' + url);
+
         const data = await response.json();
-        const urls = data.results.map(res => res.urls.raw);
+        const urls = data.results.map(res => res.urls.small);
+        //console.log('URLs:', urls[1]);
         return urls;
-    }catch (error) {
+    } catch (error) {
         console.error('Error fetching images Unsplash:', error);
         return null;
     }
