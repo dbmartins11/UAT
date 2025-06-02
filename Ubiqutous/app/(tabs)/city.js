d9e94c177afe2b8c4c1b1a9c4067e753218d1e7d
SrCoelhoPT <becas541@gmail.com>
2025-06-02 22:04:23 +0200

    feat: added support for adding cities to the lists alone

--- Ubiqutous/app/(tabs)/city.js
+++ Ubiqutous/app/(tabs)/city.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, ActivityIndicator } from 'react-native';
 import { useFonts, Merriweather_700Bold } from '@expo-google-fonts/merriweather';
 import { OpenSans_400Regular } from '@expo-google-fonts/open-sans';
@@ -9,6 +9,11 @@ import { fetchMonuments } from '../../api/serpApi.js';
 import { fetchImagesUnsplash } from '../../api/apiUnsplash.js';
 import { useNavigation } from 'expo-router';
 import BackButton from '../../components/backButton.js';
+
+import { db } from '../../firebase/firebaseConf';
+import { doc, getDoc, getDocs, setDoc, collection, updateDoc, arrayUnion} from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TouchableWithoutFeedback, Modal, TextInput } from 'react-native';
 
 export default function City() {
     const navigation = useNavigation();
@@ -24,6 +29,45 @@ export default function City() {
         OpenSans_400Regular,
         CrimsonText_400Regular,
     });
+
+
+    const [listName, setListName] = useState('');
+    const [myLists, setMyLists] = useState([]);
+    const [userID, setUserID] = useState('');
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const sidebarRef = useRef(null);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+
+    const handleBackdropPress = () => {
+        if (isSidebarOpen) {
+            setIsSidebarOpen(false);
+        }
+    };
+
+    const getLists = async () => {
+        console.log("Get Lists from" + userID);
+        if (!userID) return;
+        try {
+            console.log("Lists fetching");
+            const listsRef = collection(db, 'users', userID, 'lists');
+            const snapshot = await getDocs(listsRef);
+            const lists = [];
+            snapshot.forEach(doc => {
+                lists.push({ id: doc.id, ...doc.data() });
+            });
+            setMyLists(lists);
+        } catch (error) {
+            console.error('Error fetching lists:', error);
+        }
+    };
 
     useEffect(() => {
         setImagesReady(false);
@@ -63,6 +107,14 @@ export default function City() {
             }
         }
 
+        const getUserID = async () => {
+            const id = await AsyncStorage.getItem('userID');
+            setUserID(id || 'Unnamed');
+            console.log("User ID: ", userID);
+        };
+
+        getUserID();
+        getLists();
         fetchData(city);
         getCountryImages();
 
@@ -84,6 +136,81 @@ export default function City() {
         return <AppLoading />;
     }
 
+
+    const createList = async () => {
+        console.log("Create List");
+
+        if (!listName) {
+            console.log("List name is empty, not creating list.");
+            return;
+        }
+
+        try {
+            const userListsRef = collection(db, 'users', userID, 'lists');
+            const listDoc = doc(userListsRef, listName);
+            const docSnap = await getDoc(listDoc);
+            if (docSnap.exists()) {
+                Alert.alert('List already exists', 'Please choose a different name.');
+                return;
+            }
+            await setDoc(listDoc, {
+                name: listName,
+                countries: {
+                    [country]: {
+                        visited: false,
+                        cities: {
+                            [city]: {
+                                visited: false,
+                            }
+                        }
+                    }
+                }
+            });
+
+            console.log("List created successfully:", listName);
+            setModalVisible(false);
+            getLists();
+        } catch (error) {
+            Alert.alert('Error creating list', error.message);
+        }
+    };
+
+    const updateList = async (listName, city) => {
+        try {
+            const userListsRef = collection(db, 'users', userID, 'lists');
+            const listDoc = doc(userListsRef, listName);
+            const docSnap = await getDoc(listDoc);
+            if (docSnap.exists()) {
+                const data = docSnap.data();
+                    if (data.countries[country]) {
+                    // Country exists, add new city
+                    await updateDoc(listDoc, {
+                        [`countries.${country}.cities.${city}`]: {
+                            visited: false,
+                        }
+                    });
+                } else {
+                    // Country doesn't exist, create it
+                    await updateDoc(listDoc, {
+                        [`countries.${country}`]: {
+                            visited: false,
+                            cities: {
+                                [city]: {
+                                    visited: false,
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+            console.log("City added to list:", listName);
+            getLists();
+        } catch (error) {
+            console.error('Error updating list:', error);
+            Alert.alert('Error updating list', error.message);
+        }
+    };    
+        
     return (
         <View style={{ flex: 1, padding: 10 }}>
             {imagesReady ? (
@@ -128,20 +255,11 @@ export default function City() {
                             <Text style={styles.title}>Undefined</Text>
                         }
                         <TouchableOpacity
-                            style={{
-                                marginLeft: 30,
-                                backgroundColor: '#A0B5DB',
-                                borderRadius: 25,
-                                padding: 10,
-                            }}
-                            onPress={() => {
-                                save();
-                            }}
-                        >
+                            style={{ marginLeft: 20, borderRadius: 25, }}
+                            onPress={toggleSidebar}>
                             <Image
                                 source={require('./../../assets/icons/save.png')}
                                 style={{ width: 20, height: 20 }}
-                                resizeMode="contain"
                             />
                         </TouchableOpacity>
                     </View>
@@ -204,7 +322,109 @@ export default function City() {
                     />
                 </View>
             }
+
+
+              {isSidebarOpen && (
+                            <TouchableWithoutFeedback onPress={handleBackdropPress}>
+                                <View style={{position: 'absolute',top: 0,left: 0,right: 0,bottom: 0,backgroundColor: 'rgba(0,0,0,0.5)',justifyContent: 'flex-end',zIndex: 10}}>
+                                    <TouchableWithoutFeedback>
+                                        <View
+                                            ref={sidebarRef}
+                                            style={{
+                                                backgroundColor: '#fff',width: '70%',
+                                                height: '100%',
+                                                position: 'absolute',
+                                                right: 0,
+                                                top: 0,
+                                                padding: 20,
+                                            }}>
+                                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 20 }}>Lists</Text>
+                                            {(
+                                                myLists.map((list, idx) => (
+                                                    <TouchableOpacity
+                                                        key={idx}
+                                                        style={{ marginBottom: 10, padding: 10, backgroundColor: '#eee', borderRadius: 8 }}
+                                                        onPress={() => updateList(list.name, city)}
+                                                    >
+                                                        <Text>{list.name || `List ${idx + 1}`}</Text>
+                                                    </TouchableOpacity>
+                                                ))
+                                            )}
+                                            <TouchableOpacity
+                                                style={{
+                                                    backgroundColor: '#000',
+                                                    padding: 12,
+                                                    borderRadius: 8,
+                                                    alignItems: 'center',
+                                                    marginTop: 10,
+                                                }}
+                                                onPress={() => {modalVisible ? setModalVisible(false) : setModalVisible(true)}}>
+                                                <Modal
+                                                    animationType="fade"
+                                                    transparent={true}
+                                                    visible={modalVisible}
+                                                    onRequestClose={() => setModalVisible(false)}
+                                                >
+                                                    <View style={{
+                                                        flex: 1,
+                                                        backgroundColor: 'rgba(0,0,0,0.5)',
+                                                        justifyContent: 'center',
+                                                        alignItems: 'center'
+                                                    }}>
+                                                        <View style={{
+                                                            backgroundColor: '#fff',
+                                                            padding: 30,
+                                                            borderRadius: 12,
+                                                            alignItems: 'center',
+                                                            width: 250
+                                                        }}>
+                                                            <Text style={{ fontSize: 18, marginBottom: 20 }}>Create a new list?</Text>
+                                                                <View>
+                                                                        <TextInput
+                                                                            placeholder="List Name"
+                                                                            value={listName}
+                                                                            onChangeText={setListName}
+                                                                            style={{
+                                                                                borderWidth: 1,
+                                                                                borderColor: '#ccc',
+                                                                                borderRadius: 8,
+                                                                                padding: 10,
+                                                                                marginBottom: 10,
+                                                                                width: 150,
+                                                                            }}
+                                                                        />
+                                                                </View>
+                                                                <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                                                                    
+                                                                <TouchableOpacity
+                                                                    style={{ backgroundColor: '#000',paddingVertical: 10,paddingHorizontal: 20, borderRadius: 8,marginRight: 10,}}
+                                                                    onPress={() => {                                                                
+                                                                        createList();
+                                                                    }}>
+                                                                    <Text style={{ color: '#fff', fontWeight: 'bold' }}>Create</Text>
+                                                                </TouchableOpacity>
+            
+            
+                                                                <TouchableOpacity style={{backgroundColor: '#ccc', paddingVertical: 10,paddingHorizontal: 20,borderRadius: 8,}}
+                                                                    onPress={() => setModalVisible(false)}>
+                                                                    <Text style={{ color: '#333', fontWeight: 'bold' }}>Cancel</Text>
+                                                                </TouchableOpacity>
+            
+            
+                                                            </View>
+                                                        </View>
+                                                    </View>
+                                                </Modal>
+                                                <Text style={{ color: '#fff', fontWeight: 'bold' }}>Create New List</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        )}
         </View>
+
+        
     );
 }
 
